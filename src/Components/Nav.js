b058e4dd3df150c054dc1172f4252782e796eb69
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-04 23:27:53 +0000

    Set navlinks

---
+++ src/Components/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Nav() {
+  return (
+    <nav className="nav d-flex">
+      <h3>Book store</h3>
+      <ul className="nav-links d-flex">
+        <li>
+          <Link to="/">Book list</Link>
+        </li>
+        <li>
+          <Link to="/category">category</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
