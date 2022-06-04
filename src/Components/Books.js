373fd3c40332cd9331e8042cee399a8a178e60a6
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-04 18:34:18 +0000

    Import Form component

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Book from './Book';
+import Form from './Form';
 
 function Books() {
   return (
-    <Book />
+    <div>
+      <Book />
+      <Form />
+    </div>
   );
 }
 
