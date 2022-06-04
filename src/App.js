435548333a6238b396551359a1f794f401ac0cc2
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-04 23:26:09 +0000

    Set routes for components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Books from './Components/Books';
+import Category from './Components/Category';
+import Nav from './Components/Nav';
 
 function App() {
   return (
-    <Books />
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/category" element={<Category />} />
+      </Routes>
+    </>
   );
 }
 
