a4ffc4822753828d0fce6ae82b32bdc93552c446
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-04 16:34:15 +0000

    Create form component

---
+++ src/Components/Form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Form() {
+  return (
+    <form>
+      <input type="text" />
+      <button type="submit">ADD</button>
+    </form>
+  );
+}
+
+export default Form;
