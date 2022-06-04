f1b20afa03a0c1f75bff121f84e5a3b962b688cb
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-04 16:29:36 +0000

    Setup book component with props

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Book() {
+function Book(props) {
+  const { title, author } = props;
   return (
-    <div>Book</div>
+    <div className="book-container">
+      <div>
+        <p>{title}</p>
+        <p>{author}</p>
+      </div>
+      <button type="button">Remove</button>
+    </div>
   );
 }
+
+Book.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+};
 
 export default Book;
