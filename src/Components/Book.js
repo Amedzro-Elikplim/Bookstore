abb94bb9b4bdc9b40650d3af29055e01bcee74f0
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-08 23:57:49 +0000

    Dispatch action on remove button click

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { REMOVE } from '../redux/books/actions';
 
-function Book(props) {
-  const { title, author } = props;
+function Book({ title, author, id }) {
+  const dispatch = useDispatch();
+
+  const removeBook = () => {
+    dispatch(REMOVE(id));
+  };
+
   return (
     <div className="book-container d-flex">
       <div className="d-flex spacing container">
         <p>{title}</p>
         <p>{author}</p>
       </div>
-      <button type="button" className="button">Remove</button>
+      <button type="button" className="button" onClick={removeBook}>Remove</button>
     </div>
   );
 }
@@ -17,6 +24,7 @@ function Book(props) {
 Book.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default Book;
