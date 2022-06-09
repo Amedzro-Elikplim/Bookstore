a84058e1fd529fb03996c1d0bb6825c448ee9f39
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-09 00:42:03 +0000

    Use combineReducers

--- src/redux/ConfigureStore.js
+++ src/redux/ConfigureStore.js
@@ -1,6 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import reducer from './books/books';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import bookReducer from './books/books';
+import categoryReducer from './categories/categories';
 
+const reducer = combineReducers({ book: bookReducer, category: categoryReducer });
 const store = configureStore({ reducer });
 
 export default store;
