e19fe7ebc219d86717790cdeb80cd71ba1e75ca4
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-09 00:42:45 +0000

    Add action for CHECK_STATUS

--- src/redux/books/actions.js
+++ src/redux/books/actions.js
@@ -1,2 +1,3 @@
 export const ADD = (book) => ({ type: 'ADD_BOOK', payload: book });
-export const REMOVE = (id) => ({ type: 'REMOVE_BOOK', pa: id });
+export const REMOVE = (id) => ({ type: 'REMOVE_BOOK', payload: id });
+export const STATUS = (text) => ({ type: 'CHECK_STATUS', payload: text });
