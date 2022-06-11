f08e2fb3af5a789264cd60fef62a059f7f13373b
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-11 09:56:37 +0000

    Update api url

--- src/api.config.js
+++ src/api.config.js
@@ -1,1 +1,2 @@
-const BASEURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/co5RH6XKRZX84vte8J8F/books/'
+const BASEURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/co5RH6XKRZX84vte8J8F/books';
+export default BASEURL;
