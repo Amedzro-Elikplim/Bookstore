d34c63201e299720bf02bd92aceba4f0bdc2b980
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-06-11 17:09:03 +0000

    Remove REMOVE_BOOK method

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -5,9 +5,6 @@ const bookReducer = (state = initialState, action) => {
     case 'ADD_BOOK':
       return [...state, action.payload];
 
-    case 'REMOVE_BOOK':
-      return [...state.filter((item) => item.id !== action.payload)];
-
     case 'DISPLAY_BOOKS':
       return action.payload;
 
